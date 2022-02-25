d9f5089a1e57634745820080893b2604557319f5
pablomarcel <pablomarcel@outlook.com>
2022-02-24 20:38:48 -0800

    working on jQueryDomCrud.js

--- jQueryDomCrud.js
+++ jQueryDomCrud.js
@@ -1,6 +1,10 @@
 // Create a new <a> element containing the text "Buy Now!"
 // with an id of "cta" after the last <p>
 
+// $( document ).ready(function(){
+//     alert("code is loaded")
+// })
+
 // Access (read) the data-color attribute of the <img>,
 // log to the console
 
@@ -9,6 +13,26 @@
 
 // Remove (delete) the last paragraph
 // (starts with "Available for purchase now…")
+
+$( document ).ready(function(){
+    const $img = $('img')
+    const color = $img.data('color')
+    console.log(color)
+    const $li = $('li')
+    $li.eq(2).addClass('highlight')
+
+    const $main = $('main')
+    const $lastP = $('p')
+    const $newA = $('<a>')
+    $newA.text('Buy Now')
+    $newA.attr('id', 'cta')
+    $main.append($newA)
+
+    $lastP.remove()
+
+})
+
+
 
 // Create a listener on the "Buy Now!" link that responds to a click event.
 // When clicked, the the "Buy Now!" link should be removed
