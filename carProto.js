d3b9ef2fd67311632cd0e01fb06b0b6f2b593502
RainyCityDiver <cvmembership@posteo.net>
2022-02-24 20:54:27 -0800

    Class exercises done, finishing up jQueryDomCrud.js.

--- carProto.js
+++ carProto.js
@@ -3,13 +3,48 @@
  * @constructor
  * @param {String} model
  */
+class Car {
+    constructor ( model ) {
+        this.model = model;
+        this.currentSpeed = 0; // currentSpeed here?
+    }
+    currentSpeed = 0; // or currentSpeed here?
+    accelerate() {
+        this.currentSpeed += 1;
+        return this.currentSpeed;
+    };
+    brake() {
+        this.currentSpeed -= 1;
+        return this.currentSpeed;
+    };
+    toString() {
+        return `This is a ${this.model} moving at ${this.currentSpeed}.`
+    }
+};
+
 
 //  Create an instance, accelerate twice, brake once, and console log the instance.toString()
-
+const silverCar = new Car(Prius);
+silverCar.accelerate();
+silverCar.accelerate();
+silverCar.brake();
+console.log(silverCar.toString());
 /**
  * ElectricCar class
  * @constructor
  * @param {String} model
  */
-
+class ElectricCar extends Car {
+    constructor ( model ) {
+        super ( model );
+        this.motor = 'electric';
+    };
+    accelerate() {
+        this.accelerate();
+        this.accelerate();
+    }
+    toString() {
+        return `This ${this.motor} ${super.model} is moving at ${super.currentSpeed}`
+    }
+}
 //  Create an instance, accelerate twice, brake once, and console log the instance.toString()
--- jQueryDomCrud.js
+++ jQueryDomCrud.js
@@ -1,15 +1,33 @@
+$( document ).ready(function() {
 // Create a new <a> element containing the text "Buy Now!"
 // with an id of "cta" after the last <p>
+    let $newA = $('<a>');
+    $newA.text("Buy Now!");
+    $newA.attr('id', 'cta');
+    let $main = $('main');
+    $main.append($newA);
 
 // Access (read) the data-color attribute of the <img>,
 // log to the console
+    let $img = $('img');
+    let color = $img.data('color');
+    console.log(color);
 
 // Update the third <li> item ("Turbocharged"),
 // set the class name to "highlight"
+    let $li = $('li');
+    $li.eq(2).addClass('highlight');
 
 // Remove (delete) the last paragraph
 // (starts with "Available for purchase now…")
+    $toRemove = $('p');
+    $toRemove.remove();
 
 // Create a listener on the "Buy Now!" link that responds to a click event.
 // When clicked, the the "Buy Now!" link should be removed
 // and replaced with text that says "Added to cart"
+    $(newA).click(function(e) {
+        // need code to delete existing text (or use Kevin's technique)
+        $newA.text("Added to cart");
+    });
+});
