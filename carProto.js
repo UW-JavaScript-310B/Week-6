6122154bbdc8f560158056938138eaf7ef718557
jl105 <jilinda10@gmail.com>
2022-02-25 19:49:35 -0800

    Yuhong Week 6 Homework Solution

--- carProto.js
+++ carProto.js
@@ -3,13 +3,59 @@
  * @constructor
  * @param {String} model
  */
+ class Car {
+    constructor(currentSpeed, model) {
+        if (currentSpeed)
+            this.currentSpeed = currentSpeed;
+        else
+            this.currentSpeed = 0;
 
+        if (model)
+            this.model = model;
+        else
+            this.model = "Taurus"
+       
+    }
+    accelerate() {
+        this.currentSpeed += 1;
+    }
+    brake() {
+        if(this.currentSpeed > 0)
+            this.currentSpeed -= 1;
+        else
+            this.currentSpeed = 0;
+    }
+    toString() {
+        console.log(`${this.model} is at ${this.currentSpeed}mph.`);
+    }
+};
 //  Create an instance, accelerate twice, brake once, and console log the instance.toString()
-
+const myCar = new Car(10);
+myCar.accelerate();
+myCar.accelerate();
+myCar.brake();
+myCar.toString();
 /**
  * ElectricCar class
  * @constructor
  * @param {String} model
  */
-
+class ElectricCar extends Car{
+    constructor(currentSpeed, model) {
+        super(currentSpeed, model);
+        this.motor = "electric";
+    }
+    accelerate() {
+        super.accelerate();
+        super.accelerate();
+    }
+    toString() {
+        console.log(`Electric car ${super.toString()}` );
+    }
+}
 //  Create an instance, accelerate twice, brake once, and console log the instance.toString()
+const myElectricCar = new ElectricCar(10);
+myElectricCar.accelerate();
+myElectricCar.accelerate();
+myElectricCar.brake();
+myElectricCar.toString();
--- jQueryDomCrud.js
+++ jQueryDomCrud.js
@@ -1,15 +1,29 @@
-// Create a new <a> element containing the text "Buy Now!"
-// with an id of "cta" after the last <p>
+$( document ).ready(function() {
+    // Create a new <a> element containing the text "Buy Now!"
+    // with an id of "cta" after the last <p>
+    const $newA = $("<a>");
+    $newA.text("Buy Now!");
+    $newA.data("id", "cta");
+    $newA.attr("href", "#");
+    $("main").append($newA);
+    // Access (read) the data-color attribute of the <img>,
+    // log to the console
+    const $imgColor = $("img").data("color");
+    console.log(`img element data-color = ${$imgColor}`);
 
-// Access (read) the data-color attribute of the <img>,
-// log to the console
+    // Update the third <li> item ("Turbocharged"),
+    // set the class name to "highlight"
+    $("li").eq(2).addClass("highlight");
 
-// Update the third <li> item ("Turbocharged"),
-// set the class name to "highlight"
-
-// Remove (delete) the last paragraph
-// (starts with "Available for purchase now…")
-
-// Create a listener on the "Buy Now!" link that responds to a click event.
-// When clicked, the the "Buy Now!" link should be removed
-// and replaced with text that says "Added to cart"
+    // Remove (delete) the last paragraph
+    // (starts with "Available for purchase now…")
+    $("p").remove();
+    // Create a listener on the "Buy Now!" link that responds to a click event.
+    // When clicked, the the "Buy Now!" link should be removed
+    // and replaced with text that says "Added to cart"
+    const $newP = $("<p>").text("Added to cart");
+    $newA.on("click", function(e){
+        $newA.remove();
+        $("main").append($newP);
+    });
+});
