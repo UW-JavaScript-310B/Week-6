fdbd04048fa3e6b5e891f62644b56d6b671b8bf2
JCahill <cahilljm@aol.com>
2022-02-25 18:32:01 -0800

    Initial push of week 6 assignment

--- jQueryDomCrud.js
+++ jQueryDomCrud.js
@@ -1,15 +1,54 @@
-// Create a new <a> element containing the text "Buy Now!"
-// with an id of "cta" after the last <p>
+$(document).ready(function () {
+            // alert("Code is loaded")
 
-// Access (read) the data-color attribute of the <img>,
-// log to the console
+            // const $li = $('li');
+            // $li.addClass('NEW-CLASS')
+            // console.log($li);
+            // $li.toggleClass('something')
 
-// Update the third <li> item ("Turbocharged"),
-// set the class name to "highlight"
 
-// Remove (delete) the last paragraph
-// (starts with "Available for purchase now…")
+            // Create a new <a> element containing the text "Buy Now!"
+            // with an id of "cta" after the last <p>
 
-// Create a listener on the "Buy Now!" link that responds to a click event.
-// When clicked, the the "Buy Now!" link should be removed
-// and replaced with text that says "Added to cart"
+            const $main = $('main')
+            const $newA = $('<a>')
+            $newA.text('Buy Now!')
+            $newA.attr('id', 'cta')
+            $main.append($newA) //prepend puts it before
+
+            // Access (read) the data-color attribute of the <img>,
+            // log to the console
+            const $img = $('img');
+            const imgColor = $img.data('color');
+            console.log(imgColor);
+
+
+            // Update the third <li> item ("Turbocharged"),
+            // set the class name to "highlight"
+            const $li = $('li');
+            $li.eq(2).addClass('highlight');
+
+
+            // Remove (delete) the last paragraph
+            // (starts with "Available for purchase now…")
+
+            const $lastP = $('p');
+            $lastP.remove();
+
+            // Create a listener on the "Buy Now!" link that responds to a click event.
+            // When clicked, the the "Buy Now!" link should be removed
+            // and replaced with text that says "Added to cart"
+
+            $('a').on('click', function (e) {
+                const $buyNowA = $('a');
+                const $newP = $('<p>')
+                //remove the current link
+                $newA.remove();
+                //add a new paragraph withtext 
+                // $newP.attr('id', 'cta')
+                $newP.addClass('highlight2');
+                $newP.text('Added to Cart!')
+                $main.append($newP)
+               
+            })
+        })
--- jQueryToDoList.js
+++ jQueryToDoList.js
@@ -2,6 +2,18 @@
  * Toggles "done" class on <li> element
  */
 
+const $ulList = $('ul.today-list');
+const $liToDos = $ulList.children();
+const $li =   $('li')
+console.log($liToDos);
+
+$liToDos.on('click', function(e) {
+  let $this = $(this);
+  $(this).toggleClass('done');
+  console.log($liToDos);
+
+  })
+
 /**
  * Delete element when delete link clicked
  */
@@ -9,11 +21,33 @@
 /**
  * Adds new list item to <ul>
  */
-const addListItem = function(e) {
+const addListItem = function (e) {
   e.preventDefault();
   const text = $('input').val();
 
-  // rest here...
+  //Create new elements
+  const $newLi = $('<li>');
+  const $newSpan = $('<span>');
+  const $newA = $('<a>');
+
+
+  //add class and a descr to the delete button
+  $newA.attr('class', 'delete');
+  $newA.text('Delete');
+
+  //add text entered by user into the span el for the new list item
+  $newSpan.text(text);
+
+  //add new elements to the DOM
+  $ulList.append($newLi);
+  $newLi.append($newSpan);
+  $newLi.append($newA)
 };
 
 // add listener for add
+//identify the element to be clicked
+const $aButton = $('a.add-item')
+
+//create click event
+$aButton.on('click', addListItem)
+
