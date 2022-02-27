348f976d07cde0fdd311d2a4171da9a46f061f81
pablomarcel <pablomarcel@outlook.com>
2022-02-27 10:23:52 -0800

    completed stoppingBehavior.js.

--- stoppingBehavior.js
+++ stoppingBehavior.js
@@ -9,14 +9,25 @@ document.getElementById('cat').addEventListener('click', () => {
 // alert is working, use preventDefault to avoid fetching the link
 
 $( document ).ready(function(){
-  //alert("code is loaded")
+
   $('a').on('click', function(e){
     alert("Here is more stuff")
     e.preventDefault()
 
   })
+
+  // ise stopPropagation to avoid the 'cat' element to kick in and alert 'meow'
+
+  $('button').on('click', function(e){
+    alert("Bow wow!")
+    e.stopPropagation()
+
+  })
+
 })
 
 
 // When the bark button is clicked, should alert "Bow wow!"
 // Should *not* alert "meow"
+
+
