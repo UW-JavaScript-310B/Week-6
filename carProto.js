085bf53b40c86d0a33b955ea59bdda1c36946f7f
Joel Elizaga <joel@elizaga.org>
2022-02-28 16:13:14 -0800

    Finished exercises.

--- carProto.js
+++ carProto.js
@@ -4,7 +4,34 @@
  * @param {String} model
  */
 
+ class Car {
+    
+    constructor(model) {
+        this.currentSpeed = 0;
+        this.model = model;
+    }
+
+    accelerate() {
+        this.currentSpeed++;
+    }
+
+    decellerate() {
+        this.currentSpeed--;
+    }
+
+    toString() {
+        console.log(`My ${this.model} is going ${this.currentSpeed} fast!`);
+    }
+
+};
+
 //  Create an instance, accelerate twice, brake once, and console log the instance.toString()
+
+const tacoma = new Car("Tacoma");
+tacoma.accelerate();
+tacoma.accelerate();
+tacoma.decellerate();
+console.log(tacoma.toString());
 
 /**
  * ElectricCar class
@@ -12,4 +39,28 @@
  * @param {String} model
  */
 
+class ElectricCar extends Car {
+    
+    constructor(model) {
+        super(model);
+        this.motor = "electric";
+    }
+
+    accelerate() {
+        super.accelerate();
+        super.accelerate();
+    }
+    
+    toString() {
+        console.log(`My zippy, ${this.motor} ${this.model} is going ${this.currentSpeed} fast!`)
+    }
+
+}
+
 //  Create an instance, accelerate twice, brake once, and console log the instance.toString()
+
+const leaf = new ElectricCar("Leaf");
+leaf.accelerate();
+leaf.accelerate();
+leaf.decellerate();
+console.log(leaf.toString());
--- jQueryDomCrud.js
+++ jQueryDomCrud.js
@@ -1,15 +1,29 @@
-// Create a new <a> element containing the text "Buy Now!"
-// with an id of "cta" after the last <p>
-
-// Access (read) the data-color attribute of the <img>,
-// log to the console
-
-// Update the third <li> item ("Turbocharged"),
-// set the class name to "highlight"
-
-// Remove (delete) the last paragraph
-// (starts with "Available for purchase now…")
-
-// Create a listener on the "Buy Now!" link that responds to a click event.
-// When clicked, the the "Buy Now!" link should be removed
-// and replaced with text that says "Added to cart"
+$(document).ready(function() {
+    // Create a new <a> element containing the text "Buy Now!"
+    // with an id of "cta" after the last <p>
+    let $buyNow = $("<a>");
+    $buyNow.text("Buy Now!");
+    $buyNow.attr("id", "cta");
+    let $para = $("p");
+    $para.eq(0).after($buyNow);
+    // Access (read) the data-color attribute of the <img>,
+    // log to the console.
+    const $img = $("img");
+    const color = $img.data("color");
+    console.log(color);
+    // Update the third <li> item ("Turbocharged"),
+    // set the class name to "highlight"
+    let $li = $("li");
+    let $thirdLi = $li.eq(2);
+    $thirdLi.addClass("highlight");
+    // Remove (delete) the last paragraph
+    // (starts with "Available for purchase now…")
+    $para.eq(0).remove();
+    // Create a listener on the "Buy Now!" link that responds to a click event.
+    // When clicked, the the "Buy Now!" link should be removed
+    // and replaced with text that says "Added to cart"
+    $buyNow.on("click", function(e) {
+        $buyNow.after("Added to cart");
+        $buyNow.remove();
+    });
+});
