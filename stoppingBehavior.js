e70ffcf40ce77bfdea1357e5a1f5cdc665d27bf4
aliciagoodwin <alicia@spiltmilkware.com>
2022-02-28 19:25:24 -0800

    hw

--- stoppingBehavior.js
+++ stoppingBehavior.js
@@ -6,5 +6,20 @@ document.getElementById('cat').addEventListener('click', () => {
 // When clicked, "More info" link should alert "Here's some info"
 // instead of going to a new webpage
 
+  const $link = $('a');
+
+  $link.click(function(e) {
+    e.preventDefault();
+  alert("Here's some info");
+});
+
 // When the bark button is clicked, should alert "Bow wow!"
 // Should *not* alert "meow"
+
+  const $dog = $('button').attr('id', 'dog');
+
+    $dog.click(function(e) {
+      e.stopPropagation();
+      alert("BOW WOW");
+
+    });
