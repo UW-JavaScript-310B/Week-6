620ce27a6190af7e9bd3156a57f7bf4cc8d6f402
carlitos_206 <carloscaceres041@gmail.com>
2022-02-28 23:01:43 -0800

    partial HW

--- carProto.js
+++ carProto.js
@@ -5,6 +5,31 @@
  */
 
 //  Create an instance, accelerate twice, brake once, and console log the instance.toString()
+class Car{
+    constructor(model){
+        this.model = model
+        this.currentSpeed = 0
+    }
+    accelerate(){
+        this.currentSpeed++
+        
+    }
+    brake(){
+        this.currentSpeed--
+    }
+    toString(){
+        console.log(`The ${this.model} is at a speed of ${this.currentSpeed} MPH`)
+
+    }
+}
+
+const mustang = new Car('Mustang')
+console.log(mustang)
+mustang.accelerate();
+mustang.accelerate()
+mustang.brake()
+mustang.toString()
+
 
 /**
  * ElectricCar class
@@ -13,3 +38,15 @@
  */
 
 //  Create an instance, accelerate twice, brake once, and console log the instance.toString()
+class ElectricCar extends Car{
+    constructor(model, topSpeed, isElectric = true){
+        super(model, topSpeed)
+        this.isElectric = isElectric
+    }
+}
+const prius = new ElectricCar('Prius')
+console.log(prius)
+prius.accelerate()
+prius.accelerate()
+prius.brake()
+prius.toString()
