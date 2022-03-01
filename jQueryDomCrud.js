9ec8fbeb1c9ec8b88495e0c043aa8f0a98fbb403
Admin <admin@Admins-MacBook-Pro.local>
2022-03-01 13:49:29 -0800

    Finished DOM crud assignment

--- jQueryDomCrud.js
+++ jQueryDomCrud.js
@@ -1,15 +1,27 @@
-// Create a new <a> element containing the text "Buy Now!"
-// with an id of "cta" after the last <p>
+$(document).ready(function() {
+  // Create a new <a> element containing the text "Buy Now!"
+  // with an id of "cta" after the last <p>
+  const $anchor = $("<a>").text("Buy Now!").attr("id", "cta");
+  $("main").append($anchor);
 
-// Access (read) the data-color attribute of the <img>,
-// log to the console
+  // Access (read) the data-color attribute of the <img>,
+  // log to the console
+  console.log($("img").data("color"));
 
-// Update the third <li> item ("Turbocharged"),
-// set the class name to "highlight"
+  // Update the third <li> item ("Turbocharged"),
+  // set the class name to "highlight"
+  $("li").eq(2).addClass("highlight");
 
-// Remove (delete) the last paragraph
-// (starts with "Available for purchase now…")
+  // Remove (delete) the last paragraph
+  // (starts with "Available for purchase now…")
+  $("p").remove();
 
-// Create a listener on the "Buy Now!" link that responds to a click event.
-// When clicked, the the "Buy Now!" link should be removed
-// and replaced with text that says "Added to cart"
+  // Create a listener on the "Buy Now!" link that responds to a click event.
+  // When clicked, the the "Buy Now!" link should be removed
+  // and replaced with text that says "Added to cart"
+  const $buyButton = $("#cta");
+  $buyButton.click(() => {
+    $buyButton.remove();
+    $("main").append($("<p>").text("Added to cart"));
+  });
+});
