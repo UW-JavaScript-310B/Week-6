7794c921889455120bab5aae31a28cfb9360d59b
RainyCityDiver <cvmembership@posteo.net>
2022-03-02 13:33:22 -0800

    Quick add for troubleshooting.

--- stoppingBehavior.js
+++ stoppingBehavior.js
@@ -5,6 +5,15 @@ document.getElementById('cat').addEventListener('click', () => {
 
 // When clicked, "More info" link should alert "Here's some info"
 // instead of going to a new webpage
+// let $mInfo = 
+$('a#more-info').click(function(e) {
+  e.preventDefault();
+  alert("Here's some info");
+});
 
 // When the bark button is clicked, should alert "Bow wow!"
 // Should *not* alert "meow"
+$('#button.dog').click(function(e) {
+  e.stopPropegation();
+  alert("Bow wow!");
+})
