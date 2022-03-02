7640b4be9763f65f86401a2906c5056b68dfc14c
Admin <admin@Admins-MacBook-Pro.local>
2022-03-02 15:10:28 -0800

    Added .ready()

--- jQueryToDoList.js
+++ jQueryToDoList.js
@@ -1,35 +1,37 @@
-/**
- * Toggles "done" class on <li> element
- */
-$("ul").on("click", function(e) {
-  if($(e.target)[0].className !== 'delete') {
-    $(e.target).parent().toggleClass("done");
-  }
-});
+$(document).ready(function() {
+  /**
+   * Toggles "done" class on <li> element
+   */
+  $("ul").on("click", function(e) {
+    if($(e.target)[0].className !== 'delete') {
+      $(e.target).parent().toggleClass("done");
+    }
+  });
 
-/**
- * Delete element when delete link clicked
- */
- $("ul").on("click", function(e) {
-  if($(e.target)[0].className === 'delete') {
-    $(e.target).parent().fadeOut("slow", function() {
-      $(e.target).parent().remove();
-    });
-  }
-});
+  /**
+   * Delete element when delete link clicked
+   */
+  $("ul").on("click", function(e) {
+    if($(e.target)[0].className === 'delete') {
+      $(e.target).parent().fadeOut("slow", function() {
+        $(e.target).parent().remove();
+      });
+    }
+  });
 
-/**
- * Adds new list item to <ul>
- */
-const addListItem = function(e) {
-  e.preventDefault();
-  const listItemText = $('input').val();
-  $("ul").append(`<li><span>${listItemText}</span><a class="delete">Delete</a></li>`);
-};
+  /**
+   * Adds new list item to <ul>
+   */
+  const addListItem = function(e) {
+    e.preventDefault();
+    const listItemText = $('input').val();
+    $("ul").append(`<li><span>${listItemText}</span><a class="delete">Delete</a></li>`);
+  };
 
-$(".add-item").on("click", function(e) {
-  if(e.target.parentElement.firstElementChild.value) {
-    addListItem(e);
-    e.target.parentElement.firstElementChild.value = '';
-  }
+  $(".add-item").on("click", function(e) {
+    if(e.target.parentElement.firstElementChild.value) {
+      addListItem(e);
+      e.target.parentElement.firstElementChild.value = '';
+    }
+  });
 });
