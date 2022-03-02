e01f001cd46160988c795699901afe326a9028b9
Nell B <nellb28@yahoo.com>
2022-03-01 19:59:57 -0600

    add car class assignment

--- carProto.js
+++ carProto.js
@@ -3,7 +3,23 @@
  * @constructor
  * @param {String} model
  */
-
+class Car {
+  constructor(model) {
+    this.model = model;
+    this.speed = 0;
+    this.accelerate = function () {
+      this.speed++;
+    };
+    this.brake = function () {
+      if (this.speed > 0) {
+        this.speed--;
+      }
+    };
+  }
+  toString() {
+    console.log(`${this.model} is currently going ${this.speed} mph.`);
+  }
+}
 //  Create an instance, accelerate twice, brake once, and console log the instance.toString()
 
 /**
@@ -11,5 +27,21 @@
  * @constructor
  * @param {String} model
  */
+class ElectricCar extends Car {
+  constructor(model) {
+    super(model);
+  }
+}
 
 //  Create an instance, accelerate twice, brake once, and console log the instance.toString()
+const civic = new Car("Honda Civic");
+civic.accelerate();
+civic.accelerate();
+civic.brake();
+civic.toString();
+
+const tesla = new ElectricCar("Tesla Roadster");
+tesla.accelerate();
+tesla.accelerate();
+tesla.brake();
+tesla.toString();
