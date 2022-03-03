63880d9228c9bcde6f9df993ebd08ac2deb62cff
RainyCityDiver <cvmembership@posteo.net>
2022-03-03 14:10:22 -0800

    jQueryToDoList code tested and working.

--- jQueryToDoList.js
+++ jQueryToDoList.js
@@ -1,74 +1,57 @@
+$( document ).ready(function() {
+
 /**
  * Toggles "done" class on <li> element
  */
-$('li').click(function(e) {
+$('ul').click(function(e) {
+  $(e.target).parent().toggleClass('done');
+});
+
+/* $('li').click(function(e) {
   $(this).toggleClass('done');
-})
+}) */
+
 /**
  * Delete element when delete link clicked
  */
+
 // Includes (tested, working) extra credit.
-$('a.delete').click(function(e) {
-  $(this).parent().fadeOut('slow', function() {
-    $(this).remove();
-  });
+$('ul').click(function(e) {
+  if (e.target.className === 'delete') {
+    $(e.target).parent().fadeOut('slow', function() {
+    $(e.target).remove();
+    });
+  } 
 });
+
 /**
  * Adds new list item to <ul>
  */
 const addListItem = function(e) {
-  // e.preventDefault();
+  // commented out due to errors at runtime
+  // e.preventDefault(); 
   const text = $('input').val();
 
   // rest here...
-  /* let newDeleteBtn2 = $('<a>');
-  newDeleteBtn2.addClass('delete');
-  newDeleteBtn2.click (function (e) {
-    $(this).parent().fadeOut('slow', function() {
-      $this.remove();
-    })
-  })
-  console.log(newDeleteBtn2);
 
-  let newSpan2 = $('<span>');
-  newSpan2.append(text);
-  console.log(newSpan2);
-
-  let newLi2 = $('<li>');
-  newLi2.append(newSpan2);
-  newLi2.append(newDeleteBtn2);
-  newLi2.click(function(e) {
-    $(this).toggleClass('done');
-  })
-  console.log(newLi2); */
-
-
-
-  let $newDeleteBtn = $('<a>').addClass('delete').click(function(e) {
-    $(this).parent().remove();
+  let $newDeleteBtn = $('<a>').text('Delete').addClass('delete').click(function(e) {
+    $(e.target).parent().fadeOut('slow', function() {
+      $(e.target).remove();
   });
-  console.log($newDeleteBtn);
-
+});
+  
   let $newSpan = $('<span>').text(text);
-  console.log($newSpan);
-
+  
   let $newLi = $('<li>');
-  $newLi.append($newSpan, $newDeleteBtn).click(function(e) {
-    $(this).toggleClass('done');
-  });
-  console.log($newLi);
-};  
+  $newLi.append($newSpan, $newDeleteBtn);
+  
+  $('ul').append($newLi);
+}
 
 // add listener for add
-/* $('a.add-item').click(function (e) {
-  addListItem();
-}); */
 
-// $('a.add-item').click(addListItem);
-addListItem();
+$('a.add-item').click(function (e) {
+  addListItem(e);
+});
 
-/* $('div.add').click(function (e) {
-  let target = $('ul.today-list');
-  target.addListItem();
-}) */
-
+})
