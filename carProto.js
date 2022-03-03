ef2a93d09782056a3c9713c0d2db30512dea685f
Cahillmn <mcahill@seattleu.edu>
2022-03-02 17:17:48 -0800

    finished week 6 homework

--- carProto.js
+++ carProto.js
@@ -5,6 +5,34 @@
  */
 
 //  Create an instance, accelerate twice, brake once, and console log the instance.toString()
+class Car {
+    constructor(model) {
+        this.currentSpeed = 0;
+        this.model = model;
+        this.motor = 'Gas'
+    }
+
+    accelerate() {
+        this.currentSpeed++
+    }
+
+    decellerate() {
+        this.currentSpeed--
+    }
+
+    toString() {
+        console.log(`My ${this.model} is going ${this.currentSpeed} fast!`)
+    }
+}
+
+const impreza = new Car('Impreza');
+
+impreza.accelerate();
+impreza.accelerate();
+impreza.decellerate();
+impreza.toString();
+
+
 
 /**
  * ElectricCar class
@@ -13,3 +41,28 @@
  */
 
 //  Create an instance, accelerate twice, brake once, and console log the instance.toString()
+class ElectricCar extends Car {
+    constructor(model) {
+        super(model);
+        this.motor = 'Electric';
+    }
+
+    accelerate() {
+         super.accelerate();
+         super.accelerate();
+    }
+
+    toString() {
+        console.log(`My $80,000 ${this.model} has an ${this.motor} motor!`);
+    }
+
+
+}
+
+const tesla = new ElectricCar ('Tesla');
+console.log(tesla)
+
+tesla.accelerate();
+tesla.accelerate();
+tesla.decellerate()
+tesla.toString();
--- jQueryDomCrud.js
+++ jQueryDomCrud.js
@@ -1,15 +1,35 @@
 // Create a new <a> element containing the text "Buy Now!"
 // with an id of "cta" after the last <p>
+const $aElement = $('<a>');
+$aElement.text("Buy Now!");
+$aElement.attr('id', 'cta');
+const $mainElement = $('main');
+$mainElement.append($aElement);
 
 // Access (read) the data-color attribute of the <img>,
 // log to the console
+const $image = $('img');
+const color = $image.data('color');
+console.log(color);
 
 // Update the third <li> item ("Turbocharged"),
 // set the class name to "highlight"
+const $turbocharged = $('li');
+$turbocharged.eq(2).addClass('highlight');
+$(document).ready(function () {
+    // Remove (delete) the last paragraph
+    // (starts with "Available for purchase now…")
+    const $paragraph = $('p');
+    $paragraph.remove();
 
-// Remove (delete) the last paragraph
-// (starts with "Available for purchase now…")
+    // Create a listener on the "Buy Now!" link that responds to a click event.
+    // When clicked, the the "Buy Now!" link should be removed
+    // and replaced with text that says "Added to cart"
+    $('a').on('click', function (e) {
+        const $newParagraph = $('<p>')
+        $aElement.remove();
+        $newParagraph.text('Added to Cart!');
+        $mainElement.append($newParagraph);
 
-// Create a listener on the "Buy Now!" link that responds to a click event.
-// When clicked, the the "Buy Now!" link should be removed
-// and replaced with text that says "Added to cart"
+    })
+})
