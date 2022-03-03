9fe24f00d1b466c8e0397a29e4713046dd8143fb
pablomarcel <pablomarcel@outlook.com>
2022-03-03 05:35:41 -0800

    working on jQueryToDoList.js. completed addListItem

--- jQueryToDoList.js
+++ jQueryToDoList.js
@@ -12,9 +12,39 @@
 const addListItem = function(e) {
   e.preventDefault();
   const text = $('input').val();
-  console.log('add item clicked')
+  //console.log('add item clicked')
 
   // rest here...
+
+  const mainElem = this.parentNode.parentNode
+
+  const ulElem = mainElem.getElementsByTagName('ul')[0]
+
+  const liElem = document.createElement('li')
+
+  const spanElem = document.createElement('span')
+
+  spanElem.textContent=text
+
+  const aElem = document.createElement('a')
+
+  aElem.className="delete"
+
+  const link = document.createTextNode('Delete')
+
+  aElem.appendChild(link)
+
+  liElem.appendChild(spanElem)
+
+  liElem.appendChild(aElem)
+
+  // spanElem.addEventListener('click', doneItem)
+  //
+  // aElem.addEventListener('click', deleteListItem)
+
+  ulElem.append(liElem)
+
+
 };
 
 // add listener for add
