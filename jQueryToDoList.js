459c908dcaec2cb73cd79005a048482fd631aced
Ryan Ku <rjhk@uw.edu>
2022-03-03 19:14:28 -0800

    jQueryToDoList.js

--- jQueryToDoList.js
+++ jQueryToDoList.js
@@ -1,10 +1,26 @@
+$(document).ready(function() {
+
 /**
  * Toggles "done" class on <li> element
  */
 
+$('ul').click(function (e) {
+  if ($(e.target).prop("tagName") === 'SPAN') {
+    $(e.target).parent().toggleClass('done');
+  }
+} );
+
 /**
  * Delete element when delete link clicked
  */
+
+$('ul').click(function (e) {
+  if ($(e.target).attr('class') === 'delete') {
+    $(e.target).parent().fadeOut('normal', function () { 
+      $(e.target).parent().remove(); 
+    } );
+  }
+} );
 
 /**
  * Adds new list item to <ul>
@@ -14,6 +30,19 @@ const addListItem = function(e) {
   const text = $('input').val();
 
   // rest here...
+
+  let newLi = $('<li>');
+  let newSpan = $('<span>');
+  let newDelete = $('<a>').attr('class','delete');
+
+  newDelete.text('Delete');
+  newSpan.text(text);
+  newLi.append(newSpan);
+  newLi.append(newDelete);
+  $('ul').append(newLi);  
 };
 
 // add listener for add
+$('.add-item').click(addListItem);
+
+});
